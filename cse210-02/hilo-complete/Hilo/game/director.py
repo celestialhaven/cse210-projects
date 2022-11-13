243508679dd0166ae4d4aba1a49931d2865ce260
celestialhaven <jonzmabz04@gmail.com>
2022-11-13 13:05:14 +0800

    week02

--- cse210-02/hilo-complete/Hilo/game/director.py
+++ cse210-02/hilo-complete/Hilo/game/director.py
@@ -33,21 +33,21 @@ class Director:
             self (Director): An instance of Director.
         """
         self.cards = Hilo()
-        print(f"The card is {self.cards.card1}")
+        print(f"The card is \033[93m{self.cards.card1}\033[00m")
         
         # asking for multiplier 
-        multiply = input("Do you want to increase the bet? [y/n]: ")
+        multiply = input("\033[95mDo you want to increase the bet?\033[00m [y/n]:")
         while True:
             if multiply in ["y", "n"]:
                 break
             print("Wrong answer! [y,n] only:")
             multiply = input("Do you want to increase the bet? [y/n]: ")
         if multiply == "y":
-            self.bet = int(input("By how much? [1, 2, 3] times: "))
+            self.bet = int(input("By how much? \033[94m[1, 2, 3]\033[00m times: "))
         
         # asking for the new card if it's high or low
-        self.guess = input("Is the next card higher or lower? [h/l]: ")
-        print(f"The next card is {self.cards.card2}")
+        self.guess = input("\033[96mIs the next card higher or lower?\033[00m \033[93m[h/l]:\033[00m ")
+        print(f"The next card is \033[93m{self.cards.card2}\033[00m ")
         if self.guess == "h":
             if self.cards.card2 > self.cards.card1:
                 self.total_score += 100 * self.bet
@@ -59,13 +59,13 @@ class Director:
             else:
                 self.total_score -= 75 * self.bet
         # prints total score
-        print(f"Your score is: {self.total_score}")
+        print(f"Your score is: \033[94m{self.total_score}\033[00m")
         
         # for play agaid and breaks when the total scoe is below 0
         self.is_playing = True if self.total_score >= 0 else False
         if not self.is_playing:
             return
-        play_again = input("Play again [y/n]: ")
+        play_again = input(f"\033[92mPlay again [y/n]: \033[00m")
         while True:
             if play_again in ["y","n"]:
                 break
