0b4d65dac0da1b552d37aa29ce323590f24fd01d
celestialhaven <jonzmabz04@gmail.com>
2022-12-02 23:30:26 +0800

    week04

--- cse210-04/rfk-incomplete/rfk/game/casting/artifact.py
+++ cse210-04/rfk-incomplete/rfk/game/casting/artifact.py
@@ -3,13 +3,13 @@ from game.casting.actor import Actor
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
 
 class Artifact(Actor):
-    
+    """calling the function __init__ using super()"""
     def __init__(self):
         super().__init__()
         self._message = ""
-    
+    """set message for every artifact being found"""
     def set_message(self, message):
         self._message = message
-        
+    """returns the value of the message to window"""
     def get_message(self):
         return self._message
